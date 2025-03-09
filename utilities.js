a00ea4cd4d94a4d16c5985b25c3a143040bf3bc6
ujumairjamil <ujumairjamil@gmail.com>
2025-03-10 00:48:11 +0200

    changes

--- utilities.js
+++
@@ -1,8 +0,0 @@
-/** Get the submitted POST data from a request as an object. */
-const getData = async (request) => {
-  const body = request.body();
-  const params = await body.value;
-  return Object.fromEntries(params);
-};
-
-export { getData };
